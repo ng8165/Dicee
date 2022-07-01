6cc9b8bda866950ab8bbe5e448f093ba72770c18
Nelson Gou <nel4gou@gmail.com>
2022-06-30 23:18:04 -0700

    Swift UI Initial commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Nelson Gou on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
